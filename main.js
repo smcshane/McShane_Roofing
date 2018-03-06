c7dc239d9ba72edbde91506ee6be2d6199152c37
smcshane <32510417+smcshane@users.noreply.github.com>
2018-03-06 23:34:21 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+// When the DOM is ready, run this function
+$(document).ready(function() {
+  //Set the carousel options
+  $('#quote-carousel').carousel({
+    pause: hover,
+    interval: 4000,
+  });
+});
+
